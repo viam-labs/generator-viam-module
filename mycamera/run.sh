a4ed9040b2d9ed5a54b877b281d9aecf89492dea
Matt Vella <1242958+mcvella@users.noreply.github.com>
2023-07-19 19:18:57 -0400

    Delete mycamera directory

--- mycamera/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-
-pip install -r requirements.txt
-
-# Be sure to use `exec` so that termination signals reach the python process,
-# or handle forwarding termination signals manually
-exec python3 -m src.main $@
