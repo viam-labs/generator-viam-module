6cc9a7495d06bd48df945d94dce9e91f62905730
Matt Vella <mcvella@gmail.com>
2023-09-19 19:03:00 -0400

    use sanitized name consistently

--- generators/app/templates/python/src/__init__.py
+++ generators/app/templates/python/src/__init__.py
@@ -7,4 +7,4 @@ from viam.resource.registry import Registry, ResourceCreatorRegistration
 
 from .<%= name_sanitized %> import <%= name_sanitized %>
 
-Registry.register_resource_creator(<%= api_name %>.SUBTYPE, <%= name %>.MODEL, ResourceCreatorRegistration(<%= name %>.new, <%= name %>.validate))
+Registry.register_resource_creator(<%= api_name %>.SUBTYPE, <%= name_sanitized %>.MODEL, ResourceCreatorRegistration(<%= name_sanitized %>.new, <%= name_sanitized %>.validate))
--- generators/app/templates/python/src/__init__newapi.py
+++ generators/app/templates/python/src/__init__newapi.py
@@ -5,8 +5,8 @@ This file registers the model with the Python SDK
 from viam.resource.registry import Registry, ResourceCreatorRegistration, ResourceRegistration
 
 from .api import <%= api_name %>Client, <%= api_name %>RPCService, <%= api_name %>
-from .<%= name %> import <%= name %>
+from .<%= name_sanitized %> import <%= name_sanitized %>
 
 Registry.register_subtype(ResourceRegistration(<%= api_name %>, <%= api_name %>RPCService, lambda name, channel: <%= api_name %>Client(name, channel)))
 
-Registry.register_resource_creator(<%= api_name %>.SUBTYPE, <%= name %>.MODEL, ResourceCreatorRegistration(<%= name %>.new))
+Registry.register_resource_creator(<%= api_name %>.SUBTYPE, <%= name_sanitized %>.MODEL, ResourceCreatorRegistration(<%= name_sanitized %>.new))
